968d911eca0eb237dca34f7f50cbf9533bf16964
pj_zhong <pj_zhong@163.com>
2019-07-20 18:07:58 +0800

    SleepWaitStrategy if remove

--- src/main/java/com/z/timer/WaitStrategy.java
+++ src/main/java/com/z/timer/WaitStrategy.java
@@ -4,22 +4,20 @@ import java.util.concurrent.TimeUnit;
 
 public interface WaitStrategy {
 
-    /**
-     * Wait until the given deadline, deadlineNanoseconds
-     *
-     * @param deadlineNanoseconds deadline to wait for, in Nanoseconds
-     */
-    void waitUntil(long deadlineNanoseconds) throws InterruptedException;
+  /**
+   * Wait until the given deadline, deadlineNanoseconds
+   *
+   * @param deadlineNanoseconds deadline to wait for, in Nanoseconds
+   */
+  void waitUntil(long deadlineNanoseconds) throws InterruptedException;
 
 
-    class SleepWait implements WaitStrategy {
+  class SleepWait implements WaitStrategy {
 
-        @Override
-        public void waitUntil(long deadline) throws InterruptedException {
-            long sleepTimeNanos = deadline - System.nanoTime();
-            if (0 < sleepTimeNanos) {
-                TimeUnit.NANOSECONDS.sleep(sleepTimeNanos);
-            }
-        }
+    @Override
+    public void waitUntil(long deadline) throws InterruptedException {
+      long sleepTimeNanos = deadline - System.nanoTime();
+      TimeUnit.NANOSECONDS.sleep(sleepTimeNanos);
     }
+  }
 }
